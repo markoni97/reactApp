d3c7efaf98123704d26635cc3c9a8e46be1fd713
Marko Radenkovic <code.markoni97@gmail.com>
2021-03-17 10:25:44 +0100

    Added styled-components library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+//import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -50,7 +51,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'crimson',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -82,6 +91,7 @@ class App extends Component {
     }
 
     return (
+      //<StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -90,9 +100,11 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
+      //</StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
+//export default Radium(App);
 export default App;
